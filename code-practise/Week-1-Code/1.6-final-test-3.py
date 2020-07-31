e78a6790ace49b50b618dae5193a5bb4f5d80916
iftitutul <iftikhar.tutul@gmail.com>
2020-07-31 20:13:52 +0600

    week-1 final test

---
+++ code-practise/Week-1-Code/1.6-final-test-3.py
@@ -0,0 +1,8 @@
+# Keeping in mind there are 86400 seconds per day, write a program that calculates how many seconds there are in a week, if a week is 7 days. Print the result on the screen.
+
+# Note: Your result should be in the format of just a number, not a sentence.
+
+second = 86400
+week = 7
+calculator = second * week
+print(calculator)
