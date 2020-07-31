b06e9da5a125aeed07ec803ed956cc073a7c887d
iftitutul <iftikhar.tutul@gmail.com>
2020-07-31 19:58:20 +0600

    python

---
+++ code-practise/Week-2-Code/2.17-final-test-4.py
@@ -0,0 +1,6 @@
+# Whats the output of this code?
+
+def sum(x, y):
+	return(x+y)
+
+print(sum(sum(1, 2), sum(3, 4)))
---
+++ hackerrank/write-a-function.py
@@ -0,0 +1,16 @@
+# https://www.hackerrank.com/challenges/write-a-function/problem
+
+def is_leap(year):
+#    leap = False  
+    if (year % 4 == 0):
+       return True
+    elif (year % 400 == 0):
+       print ("leap year")
+       return True
+    elif (year % 100 == 0):
+       print ("it's not leap year")
+       return False
+    return False
+  
+year = int(input())
+print (is_leap(year))
