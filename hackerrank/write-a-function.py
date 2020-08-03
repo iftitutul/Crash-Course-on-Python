f9fe733177ae56542ab872aa4f9a66638a47df21
iftitutul <iftikhar.tutul@gmail.com>
2020-08-03 20:32:12 +0600

    fixed

--- hackerrank/write-a-function.py
+++ hackerrank/write-a-function.py
@@ -1,17 +1,18 @@
 # https://www.hackerrank.com/challenges/write-a-function/problem
 
 def is_leap(year):
-    #    leap = False
-    if (year % 4 == 0):
-       return True
-    elif (year % 400 == 0):
-       print("leap year")
-       return True
-    elif (year % 100 == 0):
-       print("it's not leap year")
-       return False
-    return False
+    leap = False
 
+    if (year % 4 == 0):
+        leap = True
+        if (year % 100 == 0):
+            leap = False
+            if (year % 400 == 0):
+                leap = True
+    return leap
+
+#def is_leap(year):
+#return year % 4 == 0 and (year % 400 == 0 or year % 100 != 0)
 
 year = int(input())
 print(is_leap(year))
