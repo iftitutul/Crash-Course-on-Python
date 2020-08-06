a3b88b85c6180d3c11d56158d00573b2b963115f
iftitutul <iftikhar.tutul@gmail.com>
2020-08-07 01:15:14 +0600

    for loop quiz + recursion

---
+++ code-practise/Week-3-Code/3.2.11-for-loop-quiz.py
@@ -0,0 +1,5 @@
+# Write a script that prints the multiples of 7 between 0 and 100. Print one multiple per line and avoid printing any numbers that aren't multiples of 7. Remember that 0 is also a multiple of 7.
+
+for x in range(0, 100):
+   if (x % 7) == 0:
+       print(x)
