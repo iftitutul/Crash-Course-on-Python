9d76b225fa8f55e5a4057ce2660bd930a2aa5a11
iftitutul <iftikhar.tutul@gmail.com>
2020-08-09 22:17:48 +0600

    week 4 string

---
+++ code-practise/Week-4-Code/4.1.2.string-index.py
@@ -0,0 +1,23 @@
+name = "Ifti"
+
+print (name[1:3])
+
+print("")
+
+print (name[0])
+print (name[1])
+print (name[2])
+print (name[3])
+
+print ("")
+
+print(name[-1])
+print(name[-2])
+print(name[-3])
+print(name[-4])
+
+print("")
+
+fruit = "pineapple"
+print (fruit[:4])
+print (fruit[4:])
