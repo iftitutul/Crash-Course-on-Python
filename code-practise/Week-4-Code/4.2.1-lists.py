1fe298d99d66b8b2e71ea5829fe93e98a861db25
iftitutul <iftikhar.tutul@gmail.com>
2020-08-13 01:03:59 +0600

    week 4 list

---
+++ code-practise/Week-4-Code/4.2.1-lists.py
@@ -0,0 +1,16 @@
+x = ["Now", "we", "are", "cooking"]
+print (type (x))
+
+print(x)
+
+y= "are" in x
+print (y)
+y = "no" in x
+print (y)
+
+print (x[0])
+print (x[-1])
+
+print (x[1:3])
+print (x[:2])
+print (x[2:])
---
+++ code-practise/Week-4-Code/4.2.3-modifying-content-of-list.py
@@ -0,0 +1,18 @@
+fruits = ["Pineapple", "Orange", "Mango", "Melon"]
+fruits.append("Kiwi")
+print (fruits)
+
+fruits.insert(3,"Malta")
+print(fruits)
+
+fruits.insert(25, "Peach")
+print(fruits)
+
+fruits.remove("Malta")
+print(fruits)
+
+print (fruits.pop(3))
+print(fruits)
+
+fruits[2] = "Strawberry"
+print(fruits)
