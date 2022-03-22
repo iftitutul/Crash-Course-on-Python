41688d7af344afe69854c84c6d2f47c6e1bc4b65
Iftikhar Hossain <iftikhar.tutul@gmail.com>
2022-03-22 12:36:02 +0600

    update

--- code-practise/Week-1-Code/1.3-calculator.py
+++ code-practise/Week-1-Code/1.3-calculator.py
@@ -8,11 +8,9 @@ print (2**5)
 print((((1+2)*3)/4)**5)
 
 
-Question 5
-
-Replace the ___ placeholder and calculate the Golden ratio:  1+52\frac{1+\sqrt{5}}{2}21+5
-​​.
-Tip: to calculate the square root of a number xxx, you can use x**(1/2).
+# Question 5
+# Replace the ___ placeholder and calculate the Golden ratio:  1+52\frac{1+\sqrt{5}}{2}21+5
+# Tips to calculate the square root of a number xxx, you can use x**(1/2).
   
 ratio = (1+(5**(1/2)))/2
 print(ratio)
