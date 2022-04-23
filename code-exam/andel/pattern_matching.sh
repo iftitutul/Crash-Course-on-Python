6cecda4bc025b1fb63b59f34aaa8f2e26d706050
Iftikhar Hossain <iftikhar.tutul@gmail.com>
2022-04-24 01:47:39 +0600

    code xm

--- code-exam/andel/pattern_matching.sh
+++ code-exam/andel/pattern_matching.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-my_array=["Abc","Ccd","Efg","def","cDe"]
+my_array=["Abc","bcd","Efg","def","cDe"]
 #readarray -t my_array </dev/stdin
 
 # Input array is read and stored in to my_array variable. 
